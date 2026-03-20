3047ce2aff587a73b26b6138957ff581c27c90c9
hazem atef <hazematefhassan1234@gmail.com>
2026-03-20 05:06:30 +0200

    Project structure and LoginPage completed; remaining pages in progress

---
+++ learn/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { LoginPage } from './components/pages/LoginPage'
+import { HomePage } from './components/pages/HomePage'
+import { RegisterPage } from './components/pages/RegisterPage'
+import { ForgetPage } from './components/pages/ForgetPage'
+import { Route , Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center font-sans">
+        {/* <LoginPage/> */}
+        <Routes>
+          <Route path='/' element={<LoginPage></LoginPage>}/>
+          <Route path='/home' element={<HomePage></HomePage>}/>
+          <Route path='/register' element={<RegisterPage></RegisterPage>}/>
+          <Route path='/forget' element={<ForgetPage></ForgetPage>}/>
+
+
+        </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ learn/src/components/pages/LoginPage.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  function handleLogin() {
+    console.log(email, password);
+    navigate("/home");
+  }
+  return (
+    <div className="w-120 h-100 text-center mb-50">
+      <h1 className="text-[50px] font-bold tracking-widest">My Tasks</h1>
+      <form
+        className="mt-15 "
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin();
+        }}
+      >
+        <h2 className="text-2xl">Login</h2>
+        <p className="mr-28.5 font-extralight mt-10 text-[1rem] opacity-50">
+          Enter Your Login Information
+        </p>
+        <input
+          type="email"
+          placeholder="Email"
+          className="border border-gray-400 w-80 h-10 px-4 rounded-lg mt-4"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border border-gray-400 w-80 h-10 px-4 rounded-lg mt-4"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+        <button onClick={()=>{
+          navigate("/forget");
+        }} type="button" className=" text-sm mt-2 block ml-42.5 underline cursor-pointer">
+          <p>Forget Your Password</p>
+        </button>
+        <button type="submit" className="w-40 h-10 rounded-lg bg-black text-white mt-6 cursor-pointer ">
+          Login
+        </button>
+        <p className="mt-8 text-sm opacity-50 underline cursor-pointer">
+          Don't have an account?{" "}
+          <span className="font-bold text-black" onClick={()=>{
+            navigate("/register");
+          }}>Register</span>
+        </p>
+      </form>
+    </div>
+  );
+};
